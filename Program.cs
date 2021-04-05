92c7b9c5f3051b30c2f7c1513aebaefcb012d5a3
NovaZure <jaytonward@yahoo.com>
2021-04-05 09:14:35 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _316TermProject
+{
+	public static class Program
+	{
+		[STAThread]
+		static void Main()
+		{
+			using (var game = new Game1())
+				game.Run();
+		}
+	}
+}
