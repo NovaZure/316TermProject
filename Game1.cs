dea4b383f78df4cdc78ebee3868839ba7dd53b9c
Adobe Wall Hacks <78327970+AdobeWallHacks@users.noreply.github.com>
2021-04-05 09:46:16 -0400

    Update Game1.cs

--- Game1.cs
+++ Game1.cs
@@ -12,6 +12,23 @@ namespace _316TermProject
 		private GraphicsDeviceManager _graphics;
 		private SpriteBatch _spriteBatch;
 
+		Vector3 cameraPos;
+		Vector3 playerPos;
+
+		float playerMovSpeed;
+		Vector3 playerJumpAccel;
+		bool isJumping = false;
+		
+		Vector3 barrelInitPos;
+		List<Vector3> barrelPos;
+
+		Model player;
+		Model barrel;
+
+		float timer;
+		bool gameOver;
+
+		KeyboardState kState;
 		public Game1()
 		{
 			_graphics = new GraphicsDeviceManager(this);
@@ -22,8 +39,14 @@ namespace _316TermProject
 		protected override void Initialize()
 		{
 			// TODO: Add your initialization logic here
-			//Fake code
-			//Fake
+			playerPos = Vector3.Zero;
+			cameraPos = new Vector3(playerPos.X, 10, 20);
+			playerMovSpeed = 0.1f;
+
+			barrelPos = new List<Vector3>();
+			barrelInitPos = new Vector3(playerPos.X + 20, 0, 0); // make the init. positive respect to player
+			timer = 0f;
+			gameOver = false;
 
 			base.Initialize();
 		}
@@ -32,7 +55,11 @@ namespace _316TermProject
 		{
 			_spriteBatch = new SpriteBatch(GraphicsDevice);
 
-			// TODO: use this.Content to load your game content here
+			//player = Content.Load<Model>("Animal_Rigged_Zebu_01");
+			//barrel = Content.Load<Model>("Barrel_Sealed_01");
+
+			//gameFont = Content.Load<SpriteFont>("Font");
+			//largeGameFont = Content.Load<SpriteFont>("LargeF");
 		}
 
 		protected override void Update(GameTime gameTime)
@@ -40,7 +67,55 @@ namespace _316TermProject
 			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
 				Exit();
 
-			// TODO: Add your update logic here
+			
+			if (!gameOver)
+			{
+				// Le funny hack to convert keypresses into data similar to joystick axes
+				Vector2 inpDir = new Vector2(
+				((kState.IsKeyDown(Keys.A)) ? -1 : 0) + ((kState.IsKeyDown(Keys.D)) ? 1 : 0),
+				((kState.IsKeyDown(Keys.W)) ? 1 : 0) + ((kState.IsKeyDown(Keys.S)) ? -1 : 0)
+				);
+
+
+				// Player left/right movement
+				Vector3 moveVec = new Vector3(playerMovSpeed, 0, 0);
+				if (kState.IsKeyDown(Keys.Left))
+				{
+					playerPos -= moveVec;
+					cameraPos -= moveVec;
+				}
+				else if (kState.IsKeyDown(Keys.Right))
+				{
+					playerPos += moveVec;
+					cameraPos += moveVec;
+				}
+
+				// Player jump calculations
+				Vector3 jumpVec = new Vector3(0, 0.356f, 0);
+				Vector3 decelerateVec = new Vector3(0, -0.009f, 0);
+				// Check for jump input, assign player velocity
+				if (kState.IsKeyDown(Keys.Space) && !isJumping)
+				{
+					isJumping = true;
+					playerJumpAccel = jumpVec;
+				}
+				// Accelerate player back down
+				if (isJumping)
+				{
+					playerPos += playerJumpAccel;
+					playerJumpAccel += decelerateVec;
+				}
+				// "Catch" the player on the floor
+				// TODO: Change this system to account for being able to stand on obstacles 
+				if (playerPos.Y < 0.0f)
+				{
+					isJumping = false;
+					playerPos.Y = 0.0f;
+				}
+
+				// update timer
+				timer += (float)gameTime.ElapsedGameTime.TotalSeconds;
+			}
 
 			base.Update(gameTime);
 		}
@@ -49,7 +124,31 @@ namespace _316TermProject
 		{
 			GraphicsDevice.Clear(Color.CornflowerBlue);
 
-			// TODO: Add your drawing code here
+			Matrix proj = Matrix.CreatePerspectiveFieldOfView(
+				MathHelper.ToRadians(60),
+				1,
+				0.001f,
+				1000f);
+
+			Matrix view = Matrix.CreateLookAt(
+				cameraPos,
+				new Vector3(playerPos.X, 0, 0),
+				new Vector3(0, 1, 0)
+				);
+
+			//SRT - scale, rotate, translate
+			Matrix world = Matrix.CreateScale(0.005f)
+				* Matrix.CreateRotationY(MathHelper.ToRadians(90))
+				* Matrix.CreateTranslation(playerPos);
+
+			//player.Draw(world, view, proj);
+
+			foreach (Vector3 pos in barrelPos)
+			{
+				world = Matrix.CreateScale(0.1f)
+					* Matrix.CreateTranslation(pos);
+				//barrel.Draw(world, view, proj);
+			}
 
 			base.Draw(gameTime);
 		}
